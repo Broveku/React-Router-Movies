ccfdfdea06bf544245de84985cb8d4ba1b01f4e1
Broveku <gearhearjordan@gmail.com>
2021-07-19 14:34:27 -0700

    completed MVP and added functionality

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 export default function MovieList(props) {
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieDetails key={movie.id} id={movie.id} movie={movie} />
       ))}
     </div>
   );
@@ -12,10 +13,15 @@ export default function MovieList(props) {
 
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
-
+  const history = useHistory()
+  
+  
+  
   return (
+    <Link to= {`/movies/${props.id}`}>
     <div className="movie-card">
-      <h2>{title}</h2>
+      <h2 >{title}</h2>
+      
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
@@ -23,5 +29,6 @@ function MovieDetails(props) {
         Metascore: <strong>{metascore}</strong>
       </div>
     </div>
+    </Link>
   );
 }
